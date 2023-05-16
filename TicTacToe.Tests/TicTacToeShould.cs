9b502137e06d3ab65f252a7e3708fbec34bc6d78
Enrique Rosales <erosales@domingoalonsogroup.com>
2023-05-16 14:15:57 +0100

    chore: created project

---
+++ TicTacToe.Tests/TicTacToeShould.cs
@@ -0,0 +1,16 @@
+namespace TicTacToe.Tests
+{
+    public class TicTacToeShould
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
