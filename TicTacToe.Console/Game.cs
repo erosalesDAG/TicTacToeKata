e12586901e0925665a05191606a18a55c564f8e7
Enrique Rosales <erosales@domingoalonsogroup.com>
2023-05-17 17:18:49 +0100

    feat: Player is unable to play twice

--- TicTacToe.Console/Game.cs
+++ TicTacToe.Console/Game.cs
@@ -2,8 +2,24 @@ namespace TicTacToe.Console;
 
 public class Game
 {
+    private string lastPlayer;
+
+    public Game()
+    {
+        lastPlayer = "";
+    }
+
     public void PlayTurn(string player)
     {
-        throw new InvalidOperationException();
+        if (lastPlayer == "" && player == "O")
+        {
+            throw new InvalidOperationException();
+        }
+        
+        if (lastPlayer != "" && lastPlayer == player)
+        {
+            throw new InvalidOperationException();
+        }
+        lastPlayer = player;
     }
 }
