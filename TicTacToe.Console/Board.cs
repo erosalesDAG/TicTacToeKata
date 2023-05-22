c84f1867857ea9c9942dd6209d4ba8589cf7c5de
Enrique Rosales <erosales@domingoalonsogroup.com>
2023-05-22 16:33:53 +0100

    feat: Unable to play twice on the same spot

--- TicTacToe.Console/Board.cs
+++ TicTacToe.Console/Board.cs
@@ -18,6 +18,7 @@ public class Board
     public void PlaceToken(Token token, Coordinates coordinates)
     {
         if (coordinates.X > currentState.GetLength(0) || coordinates.Y > currentState.GetLength(1)) throw new ArgumentOutOfRangeException();
+        if (currentState[coordinates.X, coordinates.Y] != Token.Empty) throw new InvalidOperationException();
         currentState[coordinates.X,coordinates.Y] = token;
     }
 
