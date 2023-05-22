17387c6f4b73fb9ffa7496127e169702a57de50f
Enrique Rosales <erosales@domingoalonsogroup.com>
2023-05-22 13:30:53 +0100

    refactor: extracted turn to be played verification

--- TicTacToe.Console/Game.cs
+++ TicTacToe.Console/Game.cs
@@ -11,16 +11,21 @@ public class Game
 
     public void PlayTurn(Token token)
     {
+        ValidateTurnCanBePlayed(token);
+
+        lastToken = token;
+    }
+
+    private void ValidateTurnCanBePlayed(Token token)
+    {
         if (lastToken == Token.Empty && token == Token.O)
         {
             throw new InvalidOperationException();
         }
-        
+
         if (lastToken != Token.Empty && lastToken == token)
         {
             throw new InvalidOperationException();
         }
-
-        lastToken = token;
     }
 }
